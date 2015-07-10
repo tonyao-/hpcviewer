5dca3c2651f5b92cb29d8e456ee2257b9282c544
laksono <laksono@gmail.com>
2015-07-10 21:09:58 +0000

    Create a new branch to prepare adaptation to the new Eclipse: Mars (4.5) The current viewers are based on Eclipse Indigo (3.7) dated 2011 and we are really behind the recent version of Eclipse.

---
+++ edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/scope/topdown/ScopeTreeContentProvider.java
@@ -0,0 +1,12 @@
+package edu.rice.cs.hpc.viewer.scope.topdown;
+
+import edu.rice.cs.hpc.viewer.scope.AbstractContentProvider;
+
+/************************************************************************
+ * 
+ * content provider for CCT view
+ *
+ ************************************************************************/
+public class ScopeTreeContentProvider extends AbstractContentProvider 
+{
+}
