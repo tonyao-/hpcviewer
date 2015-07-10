1e254d19f02d6a49c230f889457ea2911be0820f
laksono@gmail.com <laksono@gmail.com@f282f37c-da97-1e8e-63d0-d317b2de67e7>
2015-07-10 21:09:58 +0000

    Create a new branch to prepare adaptation to the new Eclipse: Mars (4.5) The current viewers are based on Eclipse Indigo (3.7) dated 2011 and we are really behind the recent version of Eclipse.

---
+++ branches/eclipse-mars/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/graph/GraphEditorPlot.java
@@ -0,0 +1,48 @@
+package edu.rice.cs.hpc.viewer.graph;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+import org.swtchart.IAxisSet;
+import org.swtchart.IAxisTick;
+
+import edu.rice.cs.hpc.data.experiment.metric.MetricRaw;
+import edu.rice.cs.hpc.data.experiment.scope.Scope;
+
+public class GraphEditorPlot extends GraphEditor {
+
+    public static final String ID = "edu.rice.cs.hpc.viewer.graph.GraphEditorPlot";
+    
+	@Override
+	protected double[] getValuesX(Scope scope, MetricRaw metric) 
+	throws NumberFormatException {
+
+		double []x_values = threadData.getProcessIDsDouble( metric.getID() );				
+		return x_values;
+	}
+
+	@Override
+	protected double[] getValuesY(Scope scope, MetricRaw metric) throws IOException {
+		{
+			double []y_values = threadData.getMetrics( metric, scope.getCCTIndex());
+			return y_values;
+		}
+	}
+
+
+	@Override
+	protected String getXAxisTitle() {
+		IAxisSet axisSet = this.getChart().getAxisSet();
+		IAxisTick xTick = axisSet.getXAxis(0).getTick();
+
+		xTick.setFormat(new DecimalFormat("##########"));
+
+		if (threadData.getParallelismLevel()>1) 
+		{
+			xTick.setFormat(new DecimalFormat("######00.00##"));
+		}
+
+		return threadData.getRankTitle();
+	}
+
+}
---
+++ branches/eclipse-mars/edu.rice.cs.hpc.viewer/src/edu/rice/cs/hpc/viewer/scope/flat/FlatViewContentProvider.java
@@ -0,0 +1,8 @@
+package edu.rice.cs.hpc.viewer.scope.flat;
+
+import edu.rice.cs.hpc.viewer.scope.AbstractContentProvider;
+
+public class FlatViewContentProvider extends AbstractContentProvider {
+
+
+}
